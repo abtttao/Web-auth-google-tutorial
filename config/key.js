713158103cb757a808dbd6c05fb3abbecdd90a93
student <student@dblabs-Mac-mini.local>
2020-03-10 11:47:31 +0700

    get user profile

---
+++ config/key.js
@@ -0,0 +1,7 @@
+module.exports =  {
+google: {
+    clientID: "619381067636-4loko149dcp4pg1kc51qqe4beps0h8e0.apps.googleusercontent.com" ,
+    clientSecret: "5ld3xDv4q0GO2TrMTyEilaXr"
+}
+
+}
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,15 +1,19 @@
-const passport = require ("passport");
+const passport = require("passport");
 const googleStrategy = require("passport-google-oauth").OAuth2Strategy;
 const key = require("./key")
 
 
 passport.use(
-   new googleStrategy(
-       {clientID: key.google.clientID ,
-       clientSecret: key.google.clientSecret, 
-       callbackURL: "/auth/google/redirect"
-   },
-       () => {
-//notung now
-       }
- ))
+    new googleStrategy(
+        {
+            clientID: key.google.clientID,
+            clientSecret: key.google.clientSecret,
+            callbackURL: "/auth/google/redirect"
+        },
+        (accessToken, refreshToken, profile, done) => {
+            // console.log(profile);
+            console.log(profile.displayName);
+            console.log(profile.emails[0].value);
+            console.log(profile.photos[0].value);
+        }
+    ))
